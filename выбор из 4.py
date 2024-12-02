f5a4dbedddb77cfc970c9f361c83e8fb83dfd8a7
VitaliyLitvinov <v.litvinov2000@gmail.com>
2024-12-02 18:47:46 +0300

    commit

---
+++ выбор из 4.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая определяет наименьшее из четырёх чисел.
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if a < b:
+    min_ab = a
+else:
+    min_ab = b
+if c < d:
+    min_cd = c
+else:
+    min_cd = d
+if min_ab < min_cd:
+    print(min_ab)
+else:
+    print(min_cd)

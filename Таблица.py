36a41ac553065f33526b8aba34210c9453aa2784
VitaliyLitvinov <v.litvinov2000@gmail.com>
2024-12-18 20:55:40 +0300

    commit

--- Таблица.py
+++ Таблица.py
@@ -1,7 +1,7 @@
 num = int(input())
 tmp = 1
-while tmp != num + 1:
-    for i in range(5):
-        print(tmp, end=' ')
+while tmp < num + 1:
+    for i in range(1,10):
+        print(f'{tmp} + {i} = {tmp + i}')
     tmp += 1
     print()

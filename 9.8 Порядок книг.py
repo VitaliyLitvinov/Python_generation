6c65eeedb750dff487f51a18dcdcbf8b113fd976
Vitaly_NB <v.litvinov2000@gmail.com>
2025-01-12 18:42:20 +0300

    commit

--- 9.8 Порядок книг.py
+++ 9.8 Порядок книг.py
@@ -3,23 +3,18 @@
 # Напишите программу, которая проверяет, верно ли отсортированы книги.
 
 
-flag = True
-letter = ''
-first_letter = ''
-letter_name = ''
-f_letter_name = ''
-number = int(input())
-for i in range(number):
-    string = input()
-    first_letter = string[:3]
-    index_name = string.find('«') + 1
-    f_letter_name = string[index_name]
-    if first_letter < letter:
-        flag = False
-        break
-    if first_letter == letter and f_letter_name < letter_name:
-        flag = False
-        break
-    letter_name = f_letter_name
-    letter = first_letter
-print("YES" if flag else "NO")
+n = int(input())
+previous_book = ''
+is_ordered = 'YES'
+
+for i in range(n):
+    s = input()
+    current_surname = s[: s.find(' ')]
+    current_title = s[s.find('«') + 1: s.rfind('»')]
+    current_book = current_surname + current_title
+    if current_book < previous_book:
+        is_ordered = 'NO'
+
+    previous_book = current_book
+
+print(is_ordered)
